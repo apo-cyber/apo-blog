244f479b8719f8c96ebf433c0ea24680ea5e9660
apo-cyber <poirot1948@gmail.com>
2023-01-31 10:33:00 +0900

    near

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,14 @@ if DEBUG:
     }
 else:
     DATABASES = {
-    'default': env.db(),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env("DB_NAME"),
+            'USER': env("DB_USER"),
+            'PASSWORD': env("DB_PASSWORD"),
+            'HOST': env("DB_HOST"),
+            'PORT': env("DB_PORT"),
+        }
     }
 
 
